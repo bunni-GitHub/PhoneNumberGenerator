354e7b2eac34315ee9606607f074ed4fcf4bf0d0
bunni-GitHub <85693337+bunni-GitHub@users.noreply.github.com>
2021-06-10 11:09:03 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import time
+
+time.sleep(1)
+print("please wait 5 secs for your code its generating")
+time.sleep(1)
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+time.sleep()
+print(str(random.randint(1000000000, 9999999999)))
+time.sleep(1)
+print("Enjoy")
